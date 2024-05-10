1777c30a05d85bcd0b2f10d565e5723198b84cb6
Hung22072003 <tranngocnhathung2207@gmail.com>
2024-05-10 22:47:22 +0700

    feat: crud with comment post

---
+++ src/main/java/com/example/facebookclone/DTO/CommentPostDTO.java
@@ -0,0 +1,39 @@
+package com.example.facebookclone.DTO;
+
+import com.example.facebookclone.entity.Account;
+import com.example.facebookclone.entity.Comment_Post;
+import com.example.facebookclone.entity.Post;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+public class CommentPostDTO {
+    private Integer id;
+    private String content;
+    private LocalDateTime create_time;
+    private LocalDateTime edit_time;
+    private String image;
+    private Integer id_account;
+    private Integer id_post;
+    private List<CommentPostDTO> answers;
+
+    public CommentPostDTO() {
+    }
+
+    public CommentPostDTO(Comment_Post commentPost) {
+        this.id = commentPost.getId();
+        this.content = commentPost.getContent();
+        this.create_time = commentPost.getCreate_time();
+        this.edit_time = commentPost.getEdit_time();
+        this.image = commentPost.getImage();
+        this.id_account = commentPost.getAccount().getId();
+        this.id_post = (commentPost.getPost() != null) ? commentPost.getPost().getId() : null;
+        this.answers = (commentPost.getAnswers() != null) ? commentPost.getAnswers().stream().map(CommentPostDTO::new).collect(Collectors.toList()) : List.of();
+    }
+}
---
+++ src/main/java/com/example/facebookclone/service/CommentPostService.java
@@ -0,0 +1,89 @@
+package com.example.facebookclone.service;
+
+import com.cloudinary.utils.ObjectUtils;
+import com.example.facebookclone.DTO.CommentPostDTO;
+import com.example.facebookclone.entity.Account;
+import com.example.facebookclone.entity.Comment_Post;
+import com.example.facebookclone.entity.Post;
+import com.example.facebookclone.entity.PostImage;
+import com.example.facebookclone.repository.AccountRepository;
+import com.example.facebookclone.repository.CommentPostRepository;
+import com.example.facebookclone.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class CommentPostService {
+    @Autowired
+    private CommentPostRepository commentPostRepository;
+    @Autowired
+    private AccountRepository accountRepository;
+    @Autowired
+    private PostRepository postRepository;
+    DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+    public CommentPostDTO getCommentsById(int id) {
+        return new CommentPostDTO(commentPostRepository.findById(id).get());
+    }
+
+    public List<CommentPostDTO> getCommentsByPost(int id) {
+        Optional<Post> post = postRepository.findById(id);
+        return post.get().getComments().stream().map(CommentPostDTO::new).collect(Collectors.toList());
+    }
+
+    public CommentPostDTO createComment(Integer id_account, Integer id_post, String content, MultipartFile image, Integer to_comment_id) {
+        Optional<Account> account = accountRepository.findById(id_account);
+        Comment_Post commentPost = new Comment_Post(content, new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime());
+        if(id_post != null)  {
+            Optional<Post> post = postRepository.findById(id_post);
+            commentPost.setPost(post.get());
+        }
+        commentPost.setAccount(account.get());
+
+        if(image != null) {
+            try {
+                String url = cloudinary.getInstance().uploader().upload(image.getBytes(), ObjectUtils.emptyMap()).values().toArray()[3].toString();
+                commentPost.setImage(url);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if(to_comment_id != null) {
+            Optional<Comment_Post> foundCommentPost = commentPostRepository.findById(to_comment_id);
+            foundCommentPost.get().addAnswer(commentPost);
+            commentPostRepository.save(foundCommentPost.get());
+            for(Comment_Post comment_post : foundCommentPost.get().getAnswers()) {
+                if(comment_post.getAccount() == commentPost.getAccount() && comment_post.getCreate_time() == commentPost.getCreate_time()) {
+                    return new CommentPostDTO(comment_post);
+                }
+            }
+        } else {
+            commentPostRepository.save(commentPost);
+        }
+
+
+        return new CommentPostDTO(commentPost);
+    }
+
+    public CommentPostDTO updateComment(Integer id_comment, String content) {
+        Optional<Comment_Post> foundCommentPost = commentPostRepository.findById(id_comment);
+        foundCommentPost.get().setContent(content);
+        foundCommentPost.get().setEdit_time(new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime());
+        commentPostRepository.save(foundCommentPost.get());
+        return new CommentPostDTO(foundCommentPost.get());
+    }
+
+    public void deleteComment(int id) {
+        commentPostRepository.deleteById(id);
+    }
+}
