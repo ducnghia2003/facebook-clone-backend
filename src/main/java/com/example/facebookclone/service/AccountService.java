be825d14dd16a4c37e24927ff8ada26b36faee46
Nghia <ducnghianguyen36@gmail.com>
2024-06-05 16:52:03 +0700

    feat: update avatar and cover image

--- src/main/java/com/example/facebookclone/service/AccountService.java
+++ src/main/java/com/example/facebookclone/service/AccountService.java
@@ -4,7 +4,9 @@ import com.example.facebookclone.DTO.SearchUserDTO;
 import com.example.facebookclone.DTO.UserProfileDTO;
 import com.example.facebookclone.entity.Account;
 import org.springframework.data.domain.Page;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface AccountService {
@@ -13,4 +15,6 @@ public interface AccountService {
     Page<Account> findByProfileName(String name, int limit);
     UserProfileDTO convertToUserProfileDTO(Account account);
     void updateDetailInfo(Account account);
+    void updateAvatar(MultipartFile avatar, int id);
+    void updateCoverImage(MultipartFile coverImage, int id);
 }
